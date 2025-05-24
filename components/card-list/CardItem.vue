21027e2f624f840e40ae00c5dd206f2474156774
lichingchester <liching.chester@gmail.com>
2025-05-25 00:30:47 +0800

    add dialog, and component

---
+++ components/card-list/CardItem.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { Card } from "@/types/card";
+
+defineProps<{
+  item: Card;
+}>();
+</script>
+
+<template>
+  <div>
+    <Dialog>
+      <DialogTrigger>
+        <NuxtPicture format="webp" :src="'/' + item.imagePath" loading="lazy" />
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter> Save changes </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </div>
+</template>
