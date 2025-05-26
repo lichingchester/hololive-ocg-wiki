1ca8c71b0de32fca1aee479a62c9a9e839aea92e
lichingchester <liching.chester@gmail.com>
2025-05-27 01:59:23 +0800

    add filter ui

--- components/parials/AppHeader.vue
+++ components/parials/AppHeader.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
 import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 </script>
 
 <template>
@@ -26,45 +17,6 @@ import {
         <AppLanguageSwitcher />
         <AppColorModeSwitcher />
       </div>
-    </div>
-
-    <!-- quick filters -->
-    <div class="flex items-center px-2 pb-2 gap-2">
-      <!-- color -->
-      <Select>
-        <SelectTrigger class="">
-          <SelectValue placeholder="Color" />
-        </SelectTrigger>
-        <SelectContent>
-          <template v-for="(color, key) in $tm('colors')" :key="key">
-            <SelectItem :value="key">{{ $rt(color) }}</SelectItem>
-          </template>
-        </SelectContent>
-      </Select>
-
-      <!-- CardTypeCodeType -->
-      <Select class="mr-2">
-        <SelectTrigger class="">
-          <SelectValue placeholder="Type" />
-        </SelectTrigger>
-        <SelectContent>
-          <template v-for="(type, key) in $tm('cardTypes')" :key="key">
-            <SelectItem :value="key">{{ $rt(type) }}</SelectItem>
-          </template>
-        </SelectContent>
-      </Select>
-
-      <!-- Rarity -->
-      <Select class="mr-2">
-        <SelectTrigger class="">
-          <SelectValue placeholder="Rarity" />
-        </SelectTrigger>
-        <SelectContent>
-          <template v-for="(rarity, key) in $tm('rarity')" :key="key">
-            <SelectItem :value="key">{{ $rt(rarity) }}</SelectItem>
-          </template>
-        </SelectContent>
-      </Select>
     </div>
   </header>
 </template>
