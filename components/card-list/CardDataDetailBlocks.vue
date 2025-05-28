85e0a15495759ca1a2304d744c61338f134e9a22
lichingchester <liching.chester@gmail.com>
2025-05-28 22:45:39 +0800

    update detail

--- components/card-list/CardDataDetailBlocks.vue
+++ components/card-list/CardDataDetailBlocks.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Card } from "@/types/card";
+import { ExternalLink } from "lucide-vue-next";
 
 defineProps<{
   item: Card;
@@ -160,48 +161,147 @@ const getCostTypesString = (costTypes: string[]): string => {
 
           <!-- cost -->
           <template v-if="art.costTypes">
-            <div class="flex items-center">
+            <div class="flex items-center flex-wrap">
               <template
                 v-for="(costType, costTypeIndex) in art.costTypes"
                 :key="costTypeIndex"
               >
-                <picture>
+                <picture
+                  :class="
+                    costTypeIndex === art.costTypes.length - 1 ? 'mr-1' : ''
+                  "
+                >
                   <source
                     :srcset="`/icons/arts_${costType}.webp`"
                     type="image/webp"
                   />
                   <img
-                    class="w-6 h-6"
+                    class="size-6 min-w-6 min-h-6"
                     :src="`/icons/arts_${costType}.png`"
                     loading="lazy"
                   />
                 </picture>
               </template>
 
-              <span class="ml-1 font-semibold">
-                ({{ getCostTypesString(art.costTypes) }})
-              </span>
+              <span class=""> ({{ getCostTypesString(art.costTypes) }}) </span>
             </div>
           </template>
 
-          <div class="flex gap-2">
+          <div class="flex flex-wrap gap-1 justify-end">
+            <!-- damage -->
             <Badge variant="outline" class="text-xs">
               {{ $t("fields.damage") }}:
               {{ `${art.damage}${art.isPlus ? "+" : ""}` }}
             </Badge>
+
+            <!-- specialTargets -->
+            <template v-if="art.specialTargets">
+              <template
+                v-for="(
+                  specialTarget, specialTargetIndex
+                ) in art.specialTargets"
+                :key="specialTargetIndex"
+              >
+                <Badge variant="outline" class="text-xs">
+                  <div class="flex items-center">
+                    <picture>
+                      <source
+                        :srcset="`/icons/tokkou_50_${
+                          specialTarget === 'neutral' ? 'null' : specialTarget
+                        }.webp`"
+                        type="image/webp"
+                      />
+                      <img
+                        class="w-12 min-w-8"
+                        :src="`/icons/tokkou_50_${
+                          specialTarget === 'neutral' ? 'null' : specialTarget
+                        }.png`"
+                        loading="lazy"
+                      />
+                    </picture>
+
+                    <span class="ml-1">
+                      {{ $t("fields.tokkouColor") }}:
+                      {{ $t(`colors.${specialTarget}`) }}
+                    </span>
+                  </div>
+                </Badge>
+              </template>
+            </template>
           </div>
         </div>
 
         <!-- name -->
         <div class="font-semibold">
-          {{ $t(`cards.${item.id}.keyword.name`) }}
+          {{ $t(`cards.${item.id}.arts[${index}].name`) }}
         </div>
 
         <!-- effect -->
-        <div class="">
-          {{ $t(`cards.${item.id}.keyword.effect`) }}
+        <div v-if="$te(`cards.${item.id}.arts[${index}].effect`)">
+          {{ $t(`cards.${item.id}.arts[${index}].effect`) }}
         </div>
       </div>
     </template>
   </template>
+
+  <!-- extra -->
+  <template v-if="$te(`cards.${item.id}.extra`)">
+    <div class="flex flex-col gap-2 p-2 rounded-lg border bg-accent/50">
+      <div class="flex items-center justify-between gap-2">
+        <span
+          class="text-amber-500 bg-amber-500/20 text-xs rounded-lg px-2 py-1"
+        >
+          {{ $t("fields.extra") }}
+        </span>
+      </div>
+
+      {{ $t(`cards.${item.id}.extra`) }}
+    </div>
+  </template>
+
+  <!-- abilityText -->
+  <template v-if="$te(`cards.${item.id}.abilityText`)">
+    <div class="flex flex-col gap-2 p-2 rounded-lg border bg-accent/50">
+      <div class="flex items-center justify-between gap-2">
+        <span
+          class="text-stone-800 bg-stone-800/20 dark:text-white dark:bg-white/10 text-xs rounded-lg px-2 py-1"
+        >
+          {{ $t("fields.ability") }}
+        </span>
+      </div>
+
+      <div
+        v-html="$t(`cards.${item.id}.abilityText`).replaceAll('\n', '<br>')"
+      />
+    </div>
+  </template>
+
+  <!-- illustrator -->
+  <template v-if="$te(`cards.${item.id}.illustrator`)">
+    <div class="flex flex-col gap-2 p-2 rounded-lg border bg-accent/50">
+      <div class="flex items-center justify-between gap-2">
+        <Badge variant="outline" class="text-xs">
+          {{ $t(`fields.illustrator`) }}
+        </Badge>
+
+        <div class="text-xs">
+          {{ $t(`cards.${item.id}.illustrator`) }}
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <!-- links -->
+  <div class="flex justify-end">
+    <Button variant="link" class="px-0 text-xs" as-child>
+      <a
+        :href="`https://hololive-official-cardgame.com/cardlist/?id=${item.id}`"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex items-center gap-1"
+      >
+        <ExternalLink />Official Site
+      </a>
+    </Button>
+  </div>
 </template>
