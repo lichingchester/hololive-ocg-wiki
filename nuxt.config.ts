d9ad61bcf23e05331e1d068dcb58431cf46fc319
lichingchester <liching.chester@gmail.com>
2025-05-29 23:59:29 +0800

    fix base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
         {
           rel: "icon",
           type: "image/x-icon",
-          href: "/favicon.ico",
+          href: "favicon.ico",
         },
       ],
       base: {
