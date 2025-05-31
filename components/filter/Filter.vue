338faa69206a8beef5594da71b4b963e81d657ca
lichingchester <liching.chester@gmail.com>
2025-05-31 13:52:14 +0800

    add filter options

--- components/filter/Filter.vue
+++ components/filter/Filter.vue
@@ -1,7 +1,49 @@
 <script lang="ts" setup>
-import { Funnel, PanelTopClose } from "lucide-vue-next";
+import { Funnel, PanelTopClose, RotateCcw } from "lucide-vue-next";
+import CardDataJson from "@/data/cards_i18n.json";
 
-const filterState = useFilterState();
+const { locale } = useI18n();
+
+// filter
+const filter = useFilter();
+const name = computed(() => filter.filter.value.name);
+const tag = computed(() => filter.filter.value.tag);
+const colors = computed(() => filter.filter.value.colors);
+const cardTypes = computed(() => filter.filter.value.cardTypes);
+const rarities = computed(() => filter.filter.value.rarity);
+const bloomLevel = computed(() => filter.filter.value.bloomLevel);
+
+// name filter
+const isNameOpen = ref(false);
+
+const nameList = CardDataJson.map(
+  (card) => card.translations[locale.value].name
+);
+
+const uniqueNames = new Set<string>(nameList);
+
+const nameFilterOptions = [...uniqueNames].map((name) => ({
+  value: name,
+  label: name,
+}));
+
+// tag filter
+const isTagOpen = ref(false);
+
+const tagsList = CardDataJson.map(
+  (card) => card.translations[locale.value].tags
+);
+
+const flatTagList = tagsList
+  .flat()
+  .filter((tag): tag is string => tag !== "" && tag !== undefined);
+
+const uniqueTags = new Set<string>(flatTagList);
+
+const tagFilterOptions = [...uniqueTags].map((tag) => ({
+  value: tag,
+  label: tag,
+}));
 </script>
 
 <template>
@@ -9,87 +51,229 @@ const filterState = useFilterState();
     <SheetTrigger as-child>
       <Button size="icon"> <Funnel /> </Button>
     </SheetTrigger>
-    <SheetContent side="top">
-      <!-- quick filters -->
-      <div class="flex flex-col gap-4 pt-4 px-4">
-        <!-- color -->
-        <div class="">
-          <div class="font-semibold mb-2">Colors</div>
-          <div class="flex flex-wrap gap-2">
-            <template v-for="(value, key) in filterState.colors" :key="key">
-              <Toggle
-                v-model="filterState.colors[key]"
-                size="sm"
-                variant="outline"
-                aria-label="Toggle Colors"
-              >
-                <Image
-                  :src="`/icons/type_${key}.png`"
-                  :img-attributes="{ class: 'w-4' }"
-                />
+    <SheetContent side="top" hide-top-right-close>
+      <div class="flex grow">
+        <ScrollArea>
+          <div class="w-full max-h-[calc(100dvh-68px)]">
+            <!-- quick filters -->
+            <div class="flex flex-col gap-4 pt-4 px-4">
+              <!-- name -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Name
 
-                {{ $t(`colors.${key}`) }}
-              </Toggle>
-            </template>
-          </div>
-        </div>
+                  <button @click="filter.resetName">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
 
-        <!-- CardTypeCodeType -->
-        <div class="">
-          <div class="font-semibold mb-2">Type</div>
-          <div class="flex flex-wrap gap-2">
-            <template v-for="(type, key) in filterState.cardTypes" :key="key">
-              <Toggle
-                v-model="filterState.cardTypes[key]"
-                size="sm"
-                variant="outline"
-                aria-label="Toggle Types"
-              >
-                {{ $t(`cardTypes.${key}`) }}
-              </Toggle>
-            </template>
-          </div>
-        </div>
+                <Popover v-model:open="isNameOpen">
+                  <PopoverTrigger as-child>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      class="w-[150px] justify-start"
+                    >
+                      <template v-if="name">
+                        {{ name }}
+                      </template>
+                      <template v-else> + Set Name </template>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    class="p-0"
+                    side="bottom"
+                    align="start"
+                    avoid-collisions
+                  >
+                    <Command v-model="filter.filter.value.name">
+                      <CommandInput placeholder="Change name..." />
+                      <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup>
+                          <CommandItem
+                            v-for="nameOption in nameFilterOptions"
+                            :key="nameOption.value"
+                            :value="nameOption.value"
+                            @select="
+                              () => {
+                                isNameOpen = false;
+                              }
+                            "
+                          >
+                            {{ nameOption.label }}
+                          </CommandItem>
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
 
-        <!-- Rarity -->
-        <div class="">
-          <div class="font-semibold mb-2">Rarity</div>
-          <div class="flex flex-wrap gap-2">
-            <template v-for="(rarity, key) in filterState.rarity" :key="key">
-              <Toggle
-                v-model="filterState.rarity[key]"
-                size="sm"
-                variant="outline"
-                aria-label="Toggle Rarity"
-              >
-                {{ $t(`rarity.${key}`) }}
-              </Toggle>
-            </template>
-          </div>
-        </div>
+              <!-- tag -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Tag
 
-        <!-- bloomLevel -->
-        <div class="">
-          <div class="font-semibold mb-2">Bloom Level</div>
-          <div class="flex flex-wrap gap-2">
-            <template v-for="(level, key) in filterState.bloomLevel" :key="key">
-              <Toggle
-                v-model="filterState.bloomLevel[key]"
-                size="sm"
-                variant="outline"
-                aria-label="Toggle Bloom Level"
-              >
-                {{ $t(`bloomLevel.${key}`) }}
-              </Toggle>
-            </template>
+                  <button @click="filter.resetTag">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
+
+                <Popover v-model:open="isTagOpen">
+                  <PopoverTrigger as-child>
+                    <Button
+                      variant="outline"
+                      size="sm"
+                      class="w-[150px] justify-start"
+                    >
+                      <template v-if="tag">
+                        {{ tag }}
+                      </template>
+                      <template v-else> + Set Tag </template>
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent
+                    class="p-0"
+                    side="bottom"
+                    align="start"
+                    avoid-collisions
+                  >
+                    <Command v-model="filter.filter.value.tag">
+                      <CommandInput placeholder="Change tag..." />
+                      <CommandList>
+                        <CommandEmpty>No results found.</CommandEmpty>
+                        <CommandGroup>
+                          <CommandItem
+                            v-for="tagOption in tagFilterOptions"
+                            :key="tagOption.value"
+                            :value="tagOption.value"
+                            @select="
+                              () => {
+                                isTagOpen = false;
+                              }
+                            "
+                          >
+                            {{ tagOption.label }}
+                          </CommandItem>
+                        </CommandGroup>
+                      </CommandList>
+                    </Command>
+                  </PopoverContent>
+                </Popover>
+              </div>
+
+              <!-- color -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Colors
+
+                  <button @click="filter.resetColors">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
+                <div class="flex flex-wrap gap-2">
+                  <template v-for="(value, key) in colors" :key="key">
+                    <Toggle
+                      v-model="colors[key]"
+                      size="sm"
+                      variant="outline"
+                      aria-label="Toggle Colors"
+                    >
+                      <Image
+                        :src="`/icons/type_${key}.png`"
+                        :img-attributes="{ class: 'w-4' }"
+                      />
+
+                      {{ $t(`colors.${key}`) }}
+                    </Toggle>
+                  </template>
+                </div>
+              </div>
+
+              <!-- CardTypeCodeType -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Type
+
+                  <button @click="filter.resetCardTypes">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
+                <div class="flex flex-wrap gap-2">
+                  <template v-for="(type, key) in cardTypes" :key="key">
+                    <Toggle
+                      v-model="cardTypes[key]"
+                      size="sm"
+                      variant="outline"
+                      aria-label="Toggle Types"
+                    >
+                      {{ $t(`cardTypes.${key}`) }}
+                    </Toggle>
+                  </template>
+                </div>
+              </div>
+
+              <!-- Rarity -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Rarity
+
+                  <button @click="filter.resetRarity">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
+                <div class="flex flex-wrap gap-2">
+                  <template v-for="(rarity, key) in rarities" :key="key">
+                    <Toggle
+                      v-model="rarities[key]"
+                      size="sm"
+                      variant="outline"
+                      aria-label="Toggle Rarity"
+                    >
+                      {{ $t(`rarity.${key}`) }}
+                    </Toggle>
+                  </template>
+                </div>
+              </div>
+
+              <!-- bloomLevel -->
+              <div class="">
+                <div class="flex items-center gap-2 font-semibold mb-2">
+                  Bloom Level
+
+                  <button @click="filter.resetBloomLevel">
+                    <RotateCcw class="size-4" />
+                  </button>
+                </div>
+                <div class="flex flex-wrap gap-2">
+                  <template v-for="(level, key) in bloomLevel" :key="key">
+                    <Toggle
+                      v-model="bloomLevel[key]"
+                      size="sm"
+                      variant="outline"
+                      aria-label="Toggle Bloom Level"
+                    >
+                      {{ $t(`bloomLevel.${key}`) }}
+                    </Toggle>
+                  </template>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
+        </ScrollArea>
       </div>
 
-      <SheetFooter>
-        <SheetClose as-child>
-          <Button> <PanelTopClose /> Close </Button>
-        </SheetClose>
+      <SheetFooter class="pt-0 md:pt-4">
+        <div class="flex items-center w-full gap-4">
+          <Button class="grow" variant="outline" @click="filter.reset">
+            <RotateCcw /> Reset
+          </Button>
+
+          <SheetClose as-child>
+            <Button class="grow"> <PanelTopClose /> Close </Button>
+          </SheetClose>
+        </div>
       </SheetFooter>
     </SheetContent>
   </Sheet>
--- composables/filter-states.ts
+++ composables/filter-states.ts
@@ -1,6 +1,8 @@
-export const useFilterState = () =>
-  useState("filter", () => ({
+export const useFilter = () => {
+  const filterState = useState("filter", () => ({
     search: "",
+    name: "",
+    tag: "",
     colors: {
       white: false,
       green: false,
@@ -47,3 +49,101 @@ export const useFilterState = () =>
       spot: false,
     },
   }));
+
+  const reset = () => {
+    filterState.value = {
+      search: "",
+      name: "",
+      tag: "",
+      colors: {
+        white: false,
+        green: false,
+        red: false,
+        blue: false,
+        purple: false,
+        yellow: false,
+        blue_red: false,
+        white_green: false,
+        null: false,
+      },
+      cardTypes: {
+        buzzCharacter: false,
+        character: false,
+        oshiCharacter: false,
+        supportCheer: false,
+        supportEvent: false,
+        supportEventLimited: false,
+        supportFan: false,
+        supportItem: false,
+        supportItemLimited: false,
+        supportMascot: false,
+        supportStaffLimited: false,
+      },
+      rarity: {
+        C: false,
+        OC: false,
+        OSR: false,
+        OUR: false,
+        P: false,
+        R: false,
+        RR: false,
+        S: false,
+        SEC: false,
+        SR: false,
+        SY: false,
+        U: false,
+        UR: false,
+      },
+      bloomLevel: {
+        debut: false,
+        first: false,
+        second: false,
+        spot: false,
+      },
+    };
+  };
+
+  const resetName = () => {
+    filterState.value.name = "";
+  };
+  const resetTag = () => {
+    filterState.value.tag = "";
+  };
+  const resetColors = () => {
+    Object.keys(filterState.value.colors).forEach((key) => {
+      filterState.value.colors[key as keyof typeof filterState.value.colors] =
+        false;
+    });
+  };
+  const resetCardTypes = () => {
+    Object.keys(filterState.value.cardTypes).forEach((key) => {
+      filterState.value.cardTypes[
+        key as keyof typeof filterState.value.cardTypes
+      ] = false;
+    });
+  };
+  const resetRarity = () => {
+    Object.keys(filterState.value.rarity).forEach((key) => {
+      filterState.value.rarity[key as keyof typeof filterState.value.rarity] =
+        false;
+    });
+  };
+  const resetBloomLevel = () => {
+    Object.keys(filterState.value.bloomLevel).forEach((key) => {
+      filterState.value.bloomLevel[
+        key as keyof typeof filterState.value.bloomLevel
+      ] = false;
+    });
+  };
+
+  return {
+    filter: filterState,
+    reset,
+    resetName,
+    resetTag,
+    resetColors,
+    resetCardTypes,
+    resetRarity,
+    resetBloomLevel,
+  };
+};
