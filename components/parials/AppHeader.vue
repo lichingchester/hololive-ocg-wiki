85de3cbbdccd58613962d5c03fa1f9b6f1405575
lichingchester <liching.chester@gmail.com>
2025-06-02 23:35:18 +0800

    add github link

--- components/parials/AppHeader.vue
+++ components/parials/AppHeader.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { Github } from "lucide-vue-next";
+</script>
+
 <template>
   <header class="border-solid sticky top-0 z-50 w-full border-b bg-background">
     <div class="p-2 md:p-4 flex items-center gap-2">
@@ -7,6 +11,14 @@
         <AppLanguageSwitcher />
         <AppColorModeSwitcher />
         <AppInfoButton />
+        <Button variant="ghost" size="icon" as-child>
+          <a
+            href="https://github.com/lichingchester/hololive-ocg-wiki"
+            target="_blank"
+          >
+            <Github />
+          </a>
+        </Button>
       </div>
     </div>
   </header>
