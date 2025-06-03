ff9664a80269b82677eab53927b9dc96c0a10a0c
lichingchester <liching.chester@gmail.com>
2025-06-04 01:41:57 +0800

    fix: try simple listing behaviour instead js package

--- components/card-list/CardListView.bak-virtual-scroller.vue
+++ components/card-list/CardListView.bak-virtual-scroller.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { vResizeObserver } from "@vueuse/components";
+import { RecycleScroller } from "vue-virtual-scroller";
 import type { CardCollection } from "@/types/card";
+import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
 import Fuse from "fuse.js";
 import CardDataJson from "@/data/cards_i18n.json";
 
@@ -13,6 +16,46 @@ const { locale } = useI18n();
 //   ...cardData.slice(70, 80),
 //   ...cardData.slice(400, 500),
 // ]; // Limit to the first 1000 cards for performance
+
+/**
+ * card size and padding
+ */
+let cardPadding = 8;
+const cardImageRatio =
+  (558 + cardPadding + cardPadding) / (400 + cardPadding + cardPadding); // Ratio of card height to width
+const gridColCount = shallowRef(6);
+const itemSize = shallowRef(400);
+const itemSecondarySize = shallowRef(558);
+
+function onResizeObserver(entries: ResizeObserverEntry[]) {
+  const [entry] = entries;
+  const { width } = entry.contentRect;
+
+  if (width < 640) {
+    cardPadding = 4; // Adjust ratio for smaller screens
+  } else {
+    cardPadding = 8; // Default padding for larger screens
+  }
+
+  if (width < 640) {
+    gridColCount.value = 3;
+  } else if (width < 768) {
+    gridColCount.value = 4;
+  } else if (width < 1024) {
+    gridColCount.value = 5;
+  } else if (width < 1280) {
+    gridColCount.value = 6;
+  } else if (width < 1536) {
+    gridColCount.value = 8;
+  } else if (width < 2000) {
+    gridColCount.value = 10;
+  } else {
+    gridColCount.value = 12;
+  }
+
+  itemSecondarySize.value = width / gridColCount.value; // Adjust item size based on the width of the container
+  itemSize.value = itemSecondarySize.value * cardImageRatio; // Adjust secondary size based on the item size
+}
 
 /**
  * filter functions can be added here if needed
@@ -182,12 +225,21 @@ const result = computed(() => {
 </script>
 
 <template>
-  <div
-    class="p-1 sm:p-2 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8 gap-1 sm:gap-2"
-  >
-    <template v-for="(item, index) in result" :key="index">
-      <CardItem :item="item" class="aspect-400/559" />
-    </template>
+  <div v-resize-observer="onResizeObserver" class="p-1 sm:p-2">
+    <RecycleScroller
+      class="scroller"
+      :items="result"
+      :item-size="itemSize"
+      :item-secondary-size="itemSecondarySize"
+      :grid-items="gridColCount"
+      key-field="id"
+    >
+      <template #default="{ item }">
+        <div class="p-1 sm:p-2">
+          <CardItem :item="item" />
+        </div>
+      </template>
+    </RecycleScroller>
 
     <div class="h-[65vh]"></div>
   </div>
