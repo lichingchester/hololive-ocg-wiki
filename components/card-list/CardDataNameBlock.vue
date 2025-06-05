a99c8314d20f3cf7390a8b3b35ea23e51ad8b9d2
lichingchester <liching.chester@gmail.com>
2025-06-05 22:40:35 +0800

    use native listing solution, fix badge copy

--- components/card-list/CardDataNameBlock.vue
+++ components/card-list/CardDataNameBlock.vue
@@ -37,8 +37,8 @@ const {
       </button>
       <Transition name="copied">
         <span
-          v-if="copiedName"
-          class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2"
+          v-show="copiedName"
+          class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2 whitespace-nowrap"
         >
           {{ $t("Copied") }}
         </span>
@@ -57,7 +57,7 @@ const {
         <Transition name="copied">
           <span
             v-if="copiedId"
-            class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2"
+            class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2 whitespace-nowrap"
           >
             {{ $t("Copied") }}
           </span>
@@ -75,7 +75,7 @@ const {
         <Transition name="copied">
           <span
             v-if="copiedNumber"
-            class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2"
+            class="absolute bottom-full md:bottom-auto md:top-[calc(100%+0.5rem)] left-2/4 -translate-x-2/4 -translate-y-1 rounded-lg bg-green-400 text-slate-800 text-xs py-1 px-2 whitespace-nowrap"
           >
             {{ $t("Copied") }}
           </span>
