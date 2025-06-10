78b3a7d24029c807544ee5e90aa92163abe18ca7
lichingchester <liching.chester@gmail.com>
2025-06-10 23:32:27 +0800

    update version

--- constants/app.ts
+++ constants/app.ts
@@ -1,6 +1,6 @@
 import type { Locales } from "~/types/card";
 
-export const APP_VERSION = "0.6.2";
+export const APP_VERSION = "0.7.0";
 export const APP_BASE_URL_NAME = "hololive-ocg-wiki";
 
 export const LOCALES: Locales[] = ["tc", "ja", "en", "id", "ko", "th"];
